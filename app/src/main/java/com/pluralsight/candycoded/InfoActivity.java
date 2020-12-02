a3a83cb2edbc500d6257b66398d66e28b26af77c
Shiqi Wang <seventeen@sq-mbp19.local>
2020-12-01 23:37:20 -0800

    Start phone call

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -17,25 +17,29 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 public class InfoActivity extends AppCompatActivity {
-    private static String GEO_LOCATION = "geo:0,0?q=618 E South St Orlando, FL 32801";
+    private static String GEO_LOCATION_URI = "geo:0,0?q=618 E South St Orlando, FL 32801";
     private static String MAP_PACKAGE = "com.google.android.apps.maps";
+    private static String PHONE_URI = "tel:0123456789";
 
+    View phoneText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        phoneText = findViewById(R.id.text_view_phone);
+        phoneText.setClickable(true);
+        phoneText.setOnClickListener(this::createPhoneIntent);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
-
-
     }
 
     public void createMapIntent(View view) {
-        Uri addressUri = Uri.parse(GEO_LOCATION);
+        Uri addressUri = Uri.parse(GEO_LOCATION_URI);
 
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, addressUri);
         mapIntent.setPackage(MAP_PACKAGE);
@@ -46,8 +50,9 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Intent dial = new Intent(Intent.ACTION_DIAL);
+        dial.setData(Uri.parse(PHONE_URI));
+    }
 
 }
